ad9447bde9aa55e61786c5a3737c8cc2e04cb187
sdetmharinavi <sdetmharinavi@gmail.com>
2025-11-27 22:19:10 +0530

    2025-11-27 22:19:10

--- config/table-columns/NodesTableColumns.tsx
+++ config/table-columns/NodesTableColumns.tsx
@@ -6,9 +6,10 @@ import { V_nodes_completeRowSchema } from '@/schemas/zod-schemas';
 export const NodesTableColumns = (data: V_nodes_completeRowSchema[]) => {
   return useDynamicColumnConfig('v_nodes_complete', {
     data: data,
-    omit: ['node_type_id', 'id', 'maintenance_terminal_id', 'created_at', 'updated_at'],
+    omit: ['node_type_id', 'id', 'maintenance_terminal_id', 'created_at', 'updated_at','node_type_name'],
     overrides: {
       name: {
+        sortable: true,
         render: (value: unknown) => {
           return <TruncateTooltip text={(value as string) ?? ''} className="font-semibold" />;
         },
@@ -23,12 +24,21 @@ export const NodesTableColumns = (data: V_nodes_completeRowSchema[]) => {
       },
       maintenance_area_name: {
         title: 'Maintenance Area',
+        sortable: true,
         render: (_value: unknown, record: V_nodes_completeRowSchema) => {
           const rel = record.maintenance_area_name;
           return <TruncateTooltip text={rel ?? 'N/A'} className="font-semibold" />;
         },
       },
+      remark: {
+        sortable: true,
+        editable: true,
+        render: (value: unknown) => {
+          return <TruncateTooltip text={(value as string) ?? ''} />;
+        },
+      },
       status: {
+        sortable: true,
         render: (value: unknown) => {
           return <StatusBadge status={value as string} />;
         },
