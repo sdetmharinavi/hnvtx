9ca33c490b63e7ad75a297293776fd8a788ea369
sdetmharinavi <sdetmharinavi@gmail.com>
2025-12-14 02:10:40 +0530

    2025-12-14 02:10:40

--- utils/index.ts
+++ utils/index.ts
@@ -2,7 +2,6 @@
 export { cn } from './classNames';
 export { default as formatters } from './formatters';
 export { default as validation } from './validationUtils';
-export * from './caseConverter';
 
 // Supabase
 export { createClient } from './supabase/server';
